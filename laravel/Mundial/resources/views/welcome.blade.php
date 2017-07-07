ed1e9f55d0404f4d3c92cdb01992160ceee0e770
Yuri Alexs <Yuri Alexs>
2017-07-06 22:06:07 -0300

    finish template, start SEO

--- laravel/Mundial/resources/views/welcome.blade.php
+++ laravel/Mundial/resources/views/welcome.blade.php
@@ -1,95 +1,270 @@
-<!doctype html>
-<html lang="{{ config('app.locale') }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layout.layout')
+ 
 
-        <title>Laravel</title>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+@section('content')
+     
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
 
-            .full-height {
-                height: 100vh;
-            }
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+<section class="mbr-section article" id="msg-box3-0" style="background-color: rgb(255, 255, 255); padding-top: 120px; padding-bottom: 120px;">
 
-            .position-ref {
-                position: relative;
-            }
+    
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2 text-xs-center">
+                <h3 class="mbr-section-title display-2">Sobre Nós</h3>
+                <div class="lead"><p>Fundada em outubro de 1996, a Mundial Elevadores trabalha com montagem e manutenção de elevadores residenciais e comerciais, monta-cargas, plataformas, elevadores hidráulicos e a vácuo (<a href="page2.html" class="text-danger">Elevac 200</a>).</p><p><br></p><p><br></p></div>
+                
+            </div>
+             <div class="col-md-8 col-md-offset-2 text-xs-center">
+                 
+                <img class="logo" src="{{ asset('source/images/logo.jpg') }}">
+                  
+            </div>
+        </div>
+    </div>
 
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
+</section>
 
-            .content {
-                text-align: center;
-            }
+<section class="mbr-section mbr-section__container article" id="header3-8" style="background-color: rgb(46, 46, 46); padding-top: 60px; padding-bottom: 60px;">
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <h3 class="mbr-section-title display-2">Serviços</h3>
+                <small class="mbr-section-subtitle">Alguns dos nossos serviços</small>
+            </div>
+        </div>
+    </div>
+</section>
 
-            .title {
-                font-size: 84px;
-            }
+<section class="mbr-cards mbr-section mbr-section-nopadding" id="features1-7" style="background-color: rgb(46, 46, 46);">
 
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
+        
 
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @if (Auth::check())
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ url('/login') }}">Login</a>
-                        <a href="{{ url('/register') }}">Register</a>
-                    @endif
-                </div>
-            @endif
+    <div class="mbr-cards-row row striped">
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+        <div class="mbr-cards-col col-xs-12 col-lg-4" style="padding-top: 80px; padding-bottom: 80px;">
+            <div class="container">
+                <div class="card cart-block">
+                    <div class="card-img"><img src="{{ asset('source/images/modernize-600x399.jpg') }}" class="card-img-top"></div>
+                    <div class="card-block">
+                        <h4 class="card-title">Modernização de Elevadores</h4>
+                        
+                        <p class="card-text">Renove hoje seu elevador. Economia de energia e segurança são essenciais.</p>
+                        
+                    </div>
                 </div>
             </div>
         </div>
-    </body>
-</html>
+        <div class="mbr-cards-col col-xs-12 col-lg-4" style="padding-top: 80px; padding-bottom: 80px;">
+            <div class="container">
+                <div class="card cart-block">
+                    <div class="card-img"><img src="{{ asset('source/images/elevac-600x399.jpg') }}" class="card-img-top"></div>
+                    <div class="card-block">
+                        <h4 class="card-title">Elevadores Residenciais</h4>
+                        
+                        <p class="card-text">Tenha em sua casa, escritório, buffet, salão o mais lindo e seguro elevador do mercado, o <a href="page2.html" class="text-danger">Elevac 200</a>.</p>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="mbr-cards-col col-xs-12 col-lg-4" style="padding-top: 80px; padding-bottom: 80px;">
+            <div class="container">
+                <div class="card cart-block">
+                    <div class="card-img"><img src="{{ asset('source/images/manuteno-600x399.jpg') }}" class="card-img-top"></div>
+                    <div class="card-block">
+                        <h4 class="card-title">Manutenção de Elevadores</h4>
+                        
+                        <p class="card-text">Consertos, reparos e manutenções preventivas são essenciais para prolongar a vida util do equipamento.</p>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+        
+        
+    </div>
+</section>
+
+<section class="mbr-gallery mbr-section mbr-section-nopadding mbr-slider-carousel" id="gallery3-9" data-filter="false" style="background-color: rgb(255, 255, 255); padding-top: 1.5rem; padding-bottom: 1.5rem;">
+    <!-- Filter -->
+    
+
+    <!-- Gallery -->
+    <div class="mbr-gallery-row container">
+        <div class=" mbr-gallery-layout-default">
+            <div>
+                <div>
+                    <div class="mbr-gallery-item mbr-gallery-item__mobirise3 mbr-gallery-item--p1" data-tags="Awesome" data-video-url="false">
+                        <div href="#lb-gallery3-9" data-slide-to="0" data-toggle="modal">
+                            
+                            
+
+                            <img alt="" src="{{ asset('source/images/1-2000x1335-800x534.jpg') }}">
+                            
+                            <span class="icon-focus"></span>
+                            
+                        </div>
+                    </div><div class="mbr-gallery-item mbr-gallery-item__mobirise3 mbr-gallery-item--p1" data-tags="Responsive" data-video-url="false">
+                        <div href="#lb-gallery3-9" data-slide-to="1" data-toggle="modal">
+                            
+                            
+
+                            <img alt="" src="{{ asset('source/images/2-2000x1336-800x534.jpg') }}">
+                            
+                            <span class="icon-focus"></span>
+                            
+                        </div>
+                    </div><div class="mbr-gallery-item mbr-gallery-item__mobirise3 mbr-gallery-item--p1" data-tags="Creative" data-video-url="false">
+                        <div href="#lb-gallery3-9" data-slide-to="2" data-toggle="modal">
+                            
+                            
+
+                            <img alt="" src="{{ asset('source/images/3-2000x1335-800x534.jpg') }}">
+                            
+                            <span class="icon-focus"></span>
+                            
+                        </div>
+                    </div><div class="mbr-gallery-item mbr-gallery-item__mobirise3 mbr-gallery-item--p1" data-tags="Animated" data-video-url="false">
+                        <div href="#lb-gallery3-9" data-slide-to="3" data-toggle="modal">
+                            
+                            
+
+                            <img alt="" src="{{ asset('source/images/4-2000x1335-800x534.jpg') }}">
+                            
+                            <span class="icon-focus"></span>
+                            
+                        </div>
+                    </div><div class="mbr-gallery-item mbr-gallery-item__mobirise3 mbr-gallery-item--p1" data-tags="Awesome" data-video-url="false">
+                        <div href="#lb-gallery3-9" data-slide-to="4" data-toggle="modal">
+                            
+                            
+
+                            <img alt="" src="{{ asset('source/images/5-2000x1335-800x534.jpg') }}">
+                            
+                            <span class="icon-focus"></span>
+                            
+                        </div>
+                    </div><div class="mbr-gallery-item mbr-gallery-item__mobirise3 mbr-gallery-item--p1" data-tags="Beautiful" data-video-url="false">
+                        <div href="#lb-gallery3-9" data-slide-to="5" data-toggle="modal">
+                            
+                            
+
+                            <img alt="" src="{{ asset('source/images/6-2000x1335-800x534.jpg') }}">
+                            
+                            <span class="icon-focus"></span>
+                            
+                        </div>
+                    </div><div class="mbr-gallery-item mbr-gallery-item__mobirise3 mbr-gallery-item--p1" data-tags="Responsive" data-video-url="false">
+                        <div href="#lb-gallery3-9" data-slide-to="6" data-toggle="modal">
+                            
+                            
+
+                            <img alt="" src="{{ asset('source/images/7-2000x1335-800x534.jpg') }}">
+                            
+                            <span class="icon-focus"></span>
+                            
+                        </div>
+                    </div><div class="mbr-gallery-item mbr-gallery-item__mobirise3 mbr-gallery-item--p1" data-tags="Animated" data-video-url="false">
+                        <div href="#lb-gallery3-9" data-slide-to="7" data-toggle="modal">
+                            
+                            
+
+                            <img alt="" src="{{ asset('source/images/8-2000x1335-800x534.jpg') }}">
+                            
+                            <span class="icon-focus"></span>
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="clearfix"></div>
+        </div>
+    </div>
+
+    <!-- Lightbox -->
+    <div data-app-prevent-settings="" class="mbr-slider modal fade carousel slide" tabindex="-1" data-keyboard="true" data-interval="false" id="lb-gallery3-9">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-body">
+                    <ol class="carousel-indicators">
+                        <li data-app-prevent-settings="" data-target="#lb-gallery3-9" data-slide-to="0"></li><li data-app-prevent-settings="" data-target="#lb-gallery3-9" data-slide-to="1"></li><li data-app-prevent-settings="" data-target="#lb-gallery3-9" data-slide-to="2"></li><li data-app-prevent-settings="" data-target="#lb-gallery3-9" data-slide-to="3"></li><li data-app-prevent-settings="" data-target="#lb-gallery3-9" data-slide-to="4"></li><li data-app-prevent-settings="" data-target="#lb-gallery3-9" data-slide-to="5"></li><li data-app-prevent-settings="" data-target="#lb-gallery3-9" data-slide-to="6"></li><li data-app-prevent-settings="" data-target="#lb-gallery3-9" class=" active" data-slide-to="7"></li>
+                    </ol>
+                    <div class="carousel-inner">
+                        <div class="carousel-item">
+                            <img alt="" src="{{ asset('source/images/1-2000x1335.jpg') }}">
+                        </div><div class="carousel-item">
+                            <img alt="" src="{{ asset('source/images/2-2000x1336.jpg') }}">
+                        </div><div class="carousel-item">
+                            <img alt="" src="{{ asset('source/images/3-2000x1335.jpg') }}">
+                        </div><div class="carousel-item">
+                            <img alt="" src="{{ asset('source/images/4-2000x1335.jpg') }}">
+                        </div><div class="carousel-item">
+                            <img alt="" src="{{ asset('source/images/5-2000x1335.jpg') }}">
+                        </div><div class="carousel-item">
+                            <img alt="" src="{{ asset('source/images/6-2000x1335.jpg') }}">
+                        </div><div class="carousel-item">
+                            <img alt="" src="{{ asset('source/images/7-2000x1335.jpg') }}">
+                        </div><div class="carousel-item active">
+                            <img alt="" src="{{ asset('source/images/8-2000x1335.jpg') }}">
+                        </div>
+                    </div>
+                    <a class="left carousel-control" role="button" data-slide="prev" href="#lb-gallery3-9">
+                        <span class="icon-prev" aria-hidden="true"></span>
+                        <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="right carousel-control" role="button" data-slide="next" href="#lb-gallery3-9">
+                        <span class="icon-next" aria-hidden="true"></span>
+                        <span class="sr-only">Next</span>
+                    </a>
+
+                    <a class="close" href="#" role="button" data-dismiss="modal">
+                        <span aria-hidden="true">×</span>
+                        <span class="sr-only">Close</span>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="mbr-section" id="msg-box7-q" style="background-color: rgb(30, 30, 30); padding-top: 80px; padding-bottom: 80px;">
+
+    
+    <div class="container">
+        <div class="row">
+            <div class="mbr-table-md-up">
+
+              
+
+              <div class="mbr-table-cell col-md-5 text-xs-center text-md-right content-size">
+                  <h3 class="mbr-section-title display-2">ELEVAC 200</h3>
+                  <div class="lead">
+
+                    <p>Elevador inteligente, panorâmico e muito charmoso, para novas construções e ideal para edificações já prontas. O Elevac atende de 2 a 4 andares em casas, apartamentos duplex e tríplex, escritórios e outros ambientes.</p>
+
+                  </div>
+
+                  <div><a class="btn btn-danger" href="page2.html">LEIA MAIS</a></div>
+              </div>
+
+
+              
+
+
+              <div class="mbr-table-cell mbr-left-padding-md-up mbr-valign-top col-md-7 image-size" style="width: 50%;">
+                  <div class="mbr-figure"><img src="{{ asset('source/images/2-1400x1050.jpg') }}"></div>
+              </div>
+
+            </div>
+        </div>
+    </div>
+
+</section>
+
+
+
+@endsection
